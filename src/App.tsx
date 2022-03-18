5c61e4b96fbced354795381c7a011afcb6a59515
Nemanja Paroski <paroskidev@gmail.com>
2022-03-18 18:43:19 +0100

    Fetch and filter data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import AreaChart from './components/AreaChart/AreaChart';
+import {getChartData} from './services/charts';
+import {IChart} from './typescript/models/IChart';
 
 function App() {
-  return <div className="App"></div>;
+  const [apiData, setApiData] = useState<any[]>([]);
+  const [chartData, setChartData] = useState<IChart[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getChartData();
+      const {data} = await res.json();
+
+      setApiData(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const filteredData = apiData?.filter((x: any) => x?.assetId === 'TERRA_Lido__LUNA');
+
+    setChartData(filteredData.length ? filteredData : []);
+  }, [apiData]);
+
+  return (
+    <div className="App">
+      <AreaChart data={[{xData: 0, yData: 0}]} />
+    </div>
+  );
 }
 
 export default App;
