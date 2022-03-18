1276451e17052bb4b27985a3ace19ca2b3deb4ab
Nemanja Paroski <paroskidev@gmail.com>
2022-03-18 18:42:56 +0100

    Add base area chart component.

---
+++ src/components/AreaChart/AreaChart.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Area, CartesianGrid, Tooltip, XAxis, YAxis, AreaChart as RechartsAreaChart} from 'recharts';
+
+interface IProps {
+  data: [{yData: any; xData: any}];
+}
+
+export default function AreaChart({data}: IProps) {
+  return (
+    <div>
+      <RechartsAreaChart width={730} height={250} data={data} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="xData" />
+        <YAxis dataKey="yData" />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+      </RechartsAreaChart>
+    </div>
+  );
+}
