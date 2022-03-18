ab820f63973ee7f370cf2aa02a4f619e25a70d33
Nemanja Paroski <paroskidev@gmail.com>
2022-03-18 18:58:25 +0100

    Add basic mock data for styling.

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,38 @@ function App() {
 
   return (
     <div className="App">
-      <AreaChart data={[{xData: 0, yData: 0}]} />
+      <AreaChart
+        data={[
+          {xData: 0, yData: 0, value: 12},
+          {xData: 1, yData: 1, value: 11},
+          {xData: 2, yData: 2, value: 9},
+          {xData: 3, yData: 3, value: 10},
+          {xData: 4, yData: 4, value: 7},
+          {xData: 5, yData: 5, value: 6},
+          {xData: 6, yData: 6, value: 5},
+          {xData: 7, yData: 7, value: 4},
+          {xData: 8, yData: 8, value: 3},
+          {xData: 9, yData: 9, value: 2},
+          {xData: 10, yData: 10, value: 1},
+          {xData: 11, yData: 11, value: 0.9},
+        ]}
+      />
+      <AreaChart
+        data={[
+          {xData: 0, yData: 0, value: 1},
+          {xData: 1, yData: 1, value: 1.1},
+          {xData: 2, yData: 2, value: 2.3},
+          {xData: 3, yData: 3, value: 3.3},
+          {xData: 4, yData: 4, value: 2.9},
+          {xData: 5, yData: 5, value: 3},
+          {xData: 6, yData: 6, value: 5},
+          {xData: 7, yData: 7, value: 7},
+          {xData: 8, yData: 8, value: 9},
+          {xData: 9, yData: 9, value: 6},
+          {xData: 10, yData: 10, value: 5},
+          {xData: 11, yData: 11, value: 2},
+        ]}
+      />
     </div>
   );
 }
--- src/components/AreaChart/AreaChart.tsx
+++ src/components/AreaChart/AreaChart.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Area, CartesianGrid, Tooltip, XAxis, YAxis, AreaChart as RechartsAreaChart} from 'recharts';
 
 interface IProps {
-  data: {yData: any; xData: any}[];
+  data: {yData: any; xData: any; value: any}[];
 }
 
 export default function AreaChart({data}: IProps) {
@@ -14,17 +14,12 @@ export default function AreaChart({data}: IProps) {
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
           </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-          </linearGradient>
         </defs>
         <XAxis dataKey="xData" />
         <YAxis dataKey="yData" />
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="0 0" />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        <Area type="monotone" dataKey="value" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
       </RechartsAreaChart>
     </div>
   );
